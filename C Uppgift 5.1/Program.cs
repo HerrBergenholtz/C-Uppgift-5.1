4e92b105b6b0ebea0181acd9664492c2cef1e87c
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-22 10:15:51 +0100

    Add project files.

---
+++ C Uppgift 5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_5._3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] meningar = { 
+                "Hejsan, jag heter Zacharias!",
+                "Du heter Ameen.",
+                "Trevligt att du är här idag.",
+                "Hur är det med dig?" 
+            };
+            foreach (string mening in meningar) 
+            {
+                Console.WriteLine(mening);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
